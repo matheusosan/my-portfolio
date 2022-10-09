95b8076c68aeae06a141be49eeef0c34b33ff420
Matheus <matheussantanaoli@gmail.com>
2022-10-09 19:11:22 -0300

    Ajustaco breakpoints de dispositivos.

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-import {
+import { 
   FaHtml5,
   FaCss3Alt,
   FaBootstrap,
@@ -6,17 +6,16 @@ import {
   FaSass,
 } from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
-
 import "./mainContent.css";
 
 export const Div2 = () => {
   const technologies = [
-    { id: "html", name: "HTML5", logo: <FaHtml5 /> },
-    { id: "css", name: "CSS3", logo: <FaCss3Alt /> },
-    { id: "bootstrap", name: "BootStrap", logo: <FaBootstrap /> },
-    { id: "sass", name: "SASS", logo: <FaSass /> },
-    { id: "javascript", name: "JavaScript", logo: <SiJavascript /> },
-    { id: "react", name: "ReactJS", logo: <FaReact /> },
+    { id: "html", name: "HTML5", logo: <FaHtml5 />, description: "HTML5 é uma linguagem de marcação para a World Wide Web e é uma tecnologia chave da Internet, originalmente proposto por Opera Software. É a quinta versão da linguagem HTML."},
+    { id: "css", name: "CSS3", logo: <FaCss3Alt />, description: "CSS3 é a terceira mais nova versão das famosas Cascading Style Sheets, pela qual se define estilos para um projeto web. Com efeitos de transição, imagem, imagem de fundo/background e outros, pode-se criar estilos únicos para seus projetos web, alterando diversos aspectos de design no layout da página." },
+    { id: "sass", name: "SASS", logo: <FaSass />, description: "O SASS é uma linguagem de extensão do CSS, a sigla significa “Syntactically Awesome Style Sheets” traduzindo ao pé da letra, folhas de estilo com uma sintaxe incrível. A sua ideia é adicionar recursos especiais como variáveis, mixins, funções e operações e outras opções." },
+    { id: "bootstrap", name: "BootStrap", logo: <FaBootstrap />, description: "Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web, usando HTML, CSS e JavaScript, baseado em modelos de design para a tipografia, melhorando a experiência do usuário em um site amigável e responsivo." },
+    { id: "javascript", name: "JavaScript", logo: <SiJavascript />, description: "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web." },
+    { id: "react", name: "ReactJS", logo: <FaReact />, description: "O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. É mantido pelo Facebook, Instagram, outras empresas e uma comunidade de desenvolvedores individuais. É utilizado nos sites da Netflix, Imgur, Feedly, Airbnb, SeatGeek, HelloSign, Walmart e outros." },
   ];
 
   return (
@@ -50,11 +49,7 @@ export const Div2 = () => {
               <div className="tech-about">
                 <h3>{tech.name}</h3>
                 <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Deserunt beatae quis dolorum laboriosam asperiores nesciunt
-                  neque possimus nostrum deleniti molestias maiores dolor
-                  exercitationem facere tempora, inventore sequi. Optio, ducimus
-                  ea?
+                  {tech.description}
                 </p>
               </div>
             </div>
